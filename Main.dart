f48f217d873fc2f8e70501f9952666a6dc0f6692
komal <122547611+Nezuko-Komal@users.noreply.github.com>
2024-03-28 14:51:41 +0530

    Create Main.dart

---
+++ Main.dart
@@ -0,0 +1,21 @@
+import 'package:firstapp/Screens/newapp.dart';
+import 'package:firstapp/drawer.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: newApp(),
+      
+    );
+  }
+}
